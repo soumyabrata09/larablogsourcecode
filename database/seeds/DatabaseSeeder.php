b1537411592ed1dfdbf53304b59d778b64850722
soumyabrata09 <46765055+soumyabrata09@users.noreply.github.com>
2019-01-21 21:01:18 +0530

    Update DatabaseSeeder.php
    Comments added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //c_sousen Product Tables data seeder
+        //This seeder file namely DatabaseSeeder actualy seeds data from compiling .php files <data provided to seeder file>
+        //as we have already created our ProductsTableSeeder with data hence following line will be reading data from the following mentioned class
+        //and update/seeds those data to the larablog database 
+        //make sure always to migrate and seed while interacting with tha database
          $this->call(ProductsTableSeeder::class);
     }
 }
